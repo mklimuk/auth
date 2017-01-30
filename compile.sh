57cf99898ca4a1faa8b1af01032c101b081f09b3
Michal Klimuk <michal.klimuk@neo9.pl>
2017-01-30 11:40:03 +0100

    refac(build): compile using the version file

--- compile.sh
+++ compile.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-HUSAR_VERSION="$1" docker-compose -f compile.yml run --rm auth_compile
+VER=latest
+VERSION_FILE=".version"
+
+if [[ ! -z "$1" ]]; then
+  VER="$1"
+else
+  if [[ -e $VERSION_FILE ]]; then
+    VER=`cat $VERSION_FILE`
+  fi
+fi
+
+echo "Compiling with tag: $VER"
+echo $VER > .version
+
+HUSAR_VERSION="$VER" docker-compose -f compile.yml run --rm auth_compile
 RET=$?
 exit $RET
